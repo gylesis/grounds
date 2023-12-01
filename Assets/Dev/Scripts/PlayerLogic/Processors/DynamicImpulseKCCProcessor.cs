28ec9bcb5362f453625c74b2586dd16f217d1975
Nikita Dedov <nikitadedov66@gmail.com>
2023-12-02 00:30:22 +0200

    trampoline

---
+++ Assets/Dev/Scripts/PlayerLogic/Processors/DynamicImpulseKCCProcessor.cs
@@ -0,0 +1,46 @@
+﻿using Fusion.KCC;
+using UnityEngine;
+
+namespace Dev.Scripts.PlayerLogic.Processors
+{
+    public sealed class DynamicImpulseKCCProcessor : KCCProcessor
+    {
+        // PRIVATE MEMBERS
+
+        [SerializeField]
+        private Vector3 _impulse;
+        [SerializeField]
+        private LimitKinematicVelocityKCCProcessor _limitKinematicVelocityProcessor;
+
+        // KCCProcessor INTERFACE
+
+        public override EKCCStages GetValidStages(KCC kcc, KCCData data)
+        {
+            // No KCC stage is used in this processor, we can filter them out to prevent unnecessary method calls.
+            return EKCCStages.None;
+        }
+
+        public override void OnEnter(KCC kcc, KCCData data)
+        {
+            if (_impulse.IsZero() == true)
+                return;
+
+            Vector3 rotatedImpulse = transform.rotation * _impulse;
+
+            // Clear kinematic velocity entirely
+            kcc.SetKinematicVelocity(Vector3.zero);
+
+            // Clear dynamic velocity proportionaly to impulse direction
+            kcc.SetDynamicVelocity(data.DynamicVelocity - Vector3.Scale(data.DynamicVelocity, rotatedImpulse.normalized));
+
+            // Explicitly set current position, this kills any remaining movement (CCD might be active)
+            kcc.SetPosition(data.TargetPosition);
+
+            // Add impulse
+            kcc.AddExternalImpulse(rotatedImpulse);
+
+            // Add special processor which prevents kinematic movement
+            kcc.AddModifier(_limitKinematicVelocityProcessor);
+        }
+    }
+}
