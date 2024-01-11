c23c83a1f09cdd4c541745272a8f9e77a36db750
dan2315 <danilka2315.g@gmail.com>
2024-01-11 21:47:26 +0600

    Dynamite added to Item List and Scene

--- Assets/Dev/Scripts/PlayerLogic/ImpactApplier.cs
+++ Assets/Dev/Scripts/PlayerLogic/ImpactApplier.cs
@@ -12,14 +12,14 @@ namespace Dev.Scripts.PlayerLogic
         [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
         public void RPC_ApplyInSphere(Vector3 center, float radius, float force)
         {
-            int hitCount = Runner.LagCompensation.OverlapSphere(center, radius, Runner.LocalPlayer, _hits);            
+            int hitCount = Runner.LagCompensation.OverlapSphere(center, radius, Runner.LocalPlayer, _hits);
 
             for (var i = 0; i < hitCount; i++)
             {
                 if (_hits[i].Hitbox.TryGetComponent(out NetworkRigidbody netRigidbody))
                 {
-                    netRigidbody.Rigidbody.AddForce(Vector3.up * force, ForceMode.Impulse);
-                   // netRigidbody.Rigidbody.AddExplosionForce(force, center, radius);
+                    // netRigidbody.Rigidbody.AddForce(Vector3.up * force, ForceMode.Impulse);
+                    netRigidbody.Rigidbody.AddExplosionForce(force, center, radius);
                 }
             }
         }
